0084a4dd7f18d55119fe35a9ad651adfbc23f938
Océane Patiny <oceane@patiny.com>
2020-04-20 15:49:41 +0200

    feat: add Header component

---
+++ src/components/layout/Header.js
@@ -0,0 +1,31 @@
+// function-based component! (vs class-based components)
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header style={headerStyle}>
+      <h1>To Do List</h1>
+      <Link style={linkStyle} to="/">
+        Home
+      </Link>{' '}
+      |{' '}
+      <Link style={linkStyle} to="/about">
+        About
+      </Link>
+    </header>
+  );
+}
+
+const headerStyle = {
+  background: '#334',
+  color: '#fff',
+  textAlign: 'center',
+  padding: '10px',
+};
+
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none',
+};
