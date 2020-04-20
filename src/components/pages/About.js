70bc0806115c178f7d7a9d305af6bbdaa7da1004
Océane Patiny <oceane@patiny.com>
2020-04-20 15:49:53 +0200

    feat: add About component

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// the <React.Fragment> tag is a tag that is empty -> it corresponds to nothing in the DOM
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>To Do List app v1.0.0.</p>
+      <p>
+        Developed following this tutorial:
+        https://www.youtube.com/watch?v=sBws8MSXN7A.
+      </p>
+    </React.Fragment>
+  );
+}
