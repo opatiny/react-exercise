3a80462e95418b75efd401997dcc0766d8e977a7
Océane Patiny <oceane@patiny.com>
2020-04-22 16:57:02 +0200

    chore: enhance About

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,14 +4,30 @@ import React from 'react';
 export default function About() {
   return (
     <React.Fragment>
-      <h1>About</h1>
-      <p>To Do List app v1.0.0.</p>
-      <p>
-        Developed following this tutorial:{' '}
-        <a className="link" href="https://www.youtube.com/watch?v=sBws8MSXN7A">
-          https://www.youtube.com/watch?v=sBws8MSXN7A
-        </a>
-      </p>
+      <div style={aboutStyle}>
+        <h1>About</h1>
+        <p>To Do List app</p>
+        <p>Version: v1.0.0</p>
+        <p>
+          GitHub repository:{' '}
+          <a className="link" href="https://github.com/opatiny/react-todo-list">
+            https://github.com/opatiny/react-todo-list
+          </a>
+        </p>
+        <p>
+          Developed following this tutorial:{' '}
+          <a
+            className="link"
+            href="https://www.youtube.com/watch?v=sBws8MSXN7A"
+          >
+            https://www.youtube.com/watch?v=sBws8MSXN7A
+          </a>
+        </p>
+      </div>
     </React.Fragment>
   );
 }
+
+const aboutStyle = {
+  padding: '10px',
+};
