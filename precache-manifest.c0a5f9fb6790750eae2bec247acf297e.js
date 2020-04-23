f99f94a5326d5c1be494d379829d8cd6585212d4
Océane Patiny <oceane.patiny.com>
2020-04-23 06:18:11 +0000

    Updates

---
+++ precache-manifest.c0a5f9fb6790750eae2bec247acf297e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462209b1b26f2b2469c725a337b287b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "beb698a2d782fcee0ddd",
+    "url": "./static/css/main.397b5f67.chunk.css"
+  },
+  {
+    "revision": "7f91b40cfb71d98c9975",
+    "url": "./static/js/2.175dd51f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.175dd51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb698a2d782fcee0ddd",
+    "url": "./static/js/main.ae0a97c2.chunk.js"
+  },
+  {
+    "revision": "4ee56f7179c1092d4c94",
+    "url": "./static/js/runtime-main.140d4c9b.js"
+  }
+]);
